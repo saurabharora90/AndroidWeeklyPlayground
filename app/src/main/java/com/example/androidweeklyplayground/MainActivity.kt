3eda3a3d3fd700587e54ee6deeebae8ae2fb5bbf
Saurabh Arora <saurabharora90@gmail.com>
2023-05-10 12:06:07 +0800

    Move scaffold to MainActivity to control its appearance as we go inside the nav stack

--- app/src/main/java/com/example/androidweeklyplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidweeklyplayground/MainActivity.kt
@@ -3,17 +3,65 @@ package com.example.androidweeklyplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import com.example.androidweeklyplayground.ui.theme.AndroidWeeklyPlaygroundTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
-import com.ramcosta.composedestinations.rememberNavHostEngine
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val scaffoldState = rememberScaffoldState()
+            val coroutineScope = rememberCoroutineScope()
             AndroidWeeklyPlaygroundTheme {
-                DestinationsNavHost(navGraph = NavGraphs.root)
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "Android Weekly Playground") },
+                            navigationIcon = {
+                                IconButton(onClick = {
+                                    coroutineScope.launch {
+                                        if (scaffoldState.drawerState.isClosed)
+                                            scaffoldState.drawerState.open()
+                                        else
+                                            scaffoldState.drawerState.close()
+                                    }
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Default.Menu,
+                                        contentDescription = "Menu"
+                                    )
+                                }
+                            },
+                            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+                        )
+                    },
+                    drawerContent = {
+
+                    },
+                ) { paddingValues ->
+                    DestinationsNavHost(
+                        navGraph = NavGraphs.root,
+                        modifier = Modifier.padding(paddingValues)
+                    )
+                }
             }
         }
     }
