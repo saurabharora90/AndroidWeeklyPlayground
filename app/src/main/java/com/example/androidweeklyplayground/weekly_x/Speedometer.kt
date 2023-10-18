ec270db07a5a0dc4860abbf995a298719b2f1cdb
Saurabh Arora <saurabharora90@gmail.com>
2023-10-19 00:34:47 +0800

    Update compose and speedometer implementation

--- app/src/main/java/com/example/androidweeklyplayground/weekly_x/Speedometer.kt
+++ app/src/main/java/com/example/androidweeklyplayground/weekly_x/Speedometer.kt
@@ -1,8 +1,18 @@
 package com.example.androidweeklyplayground.weekly_x
 
+import androidx.annotation.FloatRange
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -11,7 +21,6 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
@@ -20,7 +29,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import com.example.androidweeklyplayground.ui.theme.AndroidWeeklyPlaygroundTheme
+import com.ramcosta.composedestinations.annotation.Destination
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -28,13 +39,47 @@ private val INDICATOR_LENGTH = 14.dp
 private val MAJOR_INDICATOR_LENGTH = 18.dp
 private val INDICATOR_INITIAL_OFFSET = 5.dp
 
-@OptIn(ExperimentalTextApi::class)
+@Destination
 @Composable
-fun Speedometer(currentSpeed: Float, modifier: Modifier = Modifier) {
+fun SpeedometerScreen(
+    modifier: Modifier = Modifier,
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
+) {
+    Column(modifier = modifier) {
+        val speed = remember { Animatable(0f) }
+
+        Speedometer(
+            currentSpeed = speed.value,
+            modifier = Modifier
+                .padding(90.dp)
+                .requiredSize(360.dp)
+        )
+
+        Slider(
+            value = speed.value,
+            valueRange = 0f..240f,
+            onValueChange = {
+                coroutineScope.launch {
+                    speed.animateTo(
+                        it,
+                        animationSpec = tween(durationMillis = 50, easing = LinearOutSlowInEasing)
+                    )
+                }
+            },
+            modifier = Modifier.padding(16.dp),
+        )
+    }
+}
+
+@Composable
+private fun Speedometer(
+    @FloatRange(from = 0.0, to = 240.0) currentSpeed: Float,
+    modifier: Modifier = Modifier
+) {
     val textMeasurer = rememberTextMeasurer()
-    val canvasModifier = modifier
-        .requiredSize(360.dp)
-    Canvas(modifier = canvasModifier, onDraw = {
+    val textColor = MaterialTheme.colorScheme.onSurface
+    val indicatorColor = MaterialTheme.colorScheme.onSurface
+    Canvas(modifier = modifier, onDraw = {
         drawArc(
             color = Color.Red,
             startAngle = 30f,
@@ -61,8 +106,13 @@ fun Speedometer(currentSpeed: Float, modifier: Modifier = Modifier) {
                     cX = center.x,
                     cY = center.y
                 )
-                speedMarker(startOffset, markerOffset, SolidColor(Color.Black), 4.dp.toPx())
-                speedText(speed = speed, angle = angle, textMeasurer = textMeasurer)
+                speedMarker(startOffset, markerOffset, SolidColor(indicatorColor), 4.dp.toPx())
+                speedText(
+                    speed = speed,
+                    angle = angle,
+                    textMeasurer = textMeasurer,
+                    textColor = textColor
+                )
             } else if (speed % 10 == 0) {
                 val endOffset = pointOnCircle(
                     thetaInDegrees = angle.toDouble(),
@@ -81,9 +131,9 @@ fun Speedometer(currentSpeed: Float, modifier: Modifier = Modifier) {
                 speedMarker(startOffset, endOffset, SolidColor(Color.Blue), 1.dp.toPx())
             }
         }
-    })
 
-    SpeedIndicator(speedAngle = 300 - currentSpeed, modifier = canvasModifier)
+        speedIndicator(speedAngle = 300 - currentSpeed)
+    })
 }
 
 private fun DrawScope.speedMarker(
@@ -95,10 +145,10 @@ private fun DrawScope.speedMarker(
     drawLine(brush = brush, start = startPoint, end = endPoint, strokeWidth = strokeWidth)
 }
 
-@OptIn(ExperimentalTextApi::class)
 private fun DrawScope.speedText(
     speed: Int,
     angle: Int,
+    textColor: Color,
     textMeasurer: TextMeasurer
 ) {
     val textLayoutResult = textMeasurer.measure(
@@ -107,49 +157,44 @@ private fun DrawScope.speedText(
     )
     val textWidth = textLayoutResult.size.width
     val textHeight = textLayoutResult.size.height
-    val angleSpan = (textWidth / (size.height / 2 - MAJOR_INDICATOR_LENGTH.toPx())).toDouble()
-    val textAngle = angle - angleSpan / 2
 
     val textOffset = pointOnCircle(
-        thetaInDegrees = textAngle,
+        thetaInDegrees = angle.toDouble(),
         radius = size.height / 2 - MAJOR_INDICATOR_LENGTH.toPx() - textWidth / 2 - INDICATOR_INITIAL_OFFSET.toPx(), // Adjusting radius with text width
         cX = center.x,
         cY = center.y
     )
 
     drawContext.canvas.save()
+    // Translate to the text offset point, adjusting for vertical centering.
     drawContext.canvas.translate(
         textOffset.x - textWidth / 2,
-        textOffset.y - textHeight / 2 // Adjust the y position for vertical centering
+        textOffset.y - textHeight / 2
     )
 
-    drawText(textLayoutResult)
+    drawText(textLayoutResult, color = textColor)
 
     drawContext.canvas.restore()
 }
 
-@Composable
-private fun SpeedIndicator(
-    speedAngle: Float,
-    modifier: Modifier = Modifier
+private fun DrawScope.speedIndicator(
+    speedAngle: Float
 ) {
-    Canvas(modifier = modifier, onDraw = {
-        val endOffset = pointOnCircle(
-            thetaInDegrees = speedAngle.toDouble(),
-            radius = size.height / 2 - INDICATOR_LENGTH.toPx(),
-            cX = center.x,
-            cY = center.y
-        )
+    val endOffset = pointOnCircle(
+        thetaInDegrees = speedAngle.toDouble(),
+        radius = size.height / 2 - INDICATOR_LENGTH.toPx(),
+        cX = center.x,
+        cY = center.y
+    )
 
-        drawLine(
-            color = Color.Magenta,
-            start = center,
-            end = endOffset,
-            strokeWidth = 6.dp.toPx(),
-            cap = StrokeCap.Round,
-            alpha = 0.5f
-        )
-    })
+    drawLine(
+        color = Color.Magenta,
+        start = center,
+        end = endOffset,
+        strokeWidth = 6.dp.toPx(),
+        cap = StrokeCap.Round,
+        alpha = 0.5f
+    )
 }
 
 private fun pointOnCircle(
@@ -167,7 +212,5 @@ private fun pointOnCircle(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun SpeedometerPreview() {
-    AndroidWeeklyPlaygroundTheme {
-        Speedometer(currentSpeed = 120f)
-    }
+    SpeedometerScreen()
 }
